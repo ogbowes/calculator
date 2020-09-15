92f74b1609a9ea48306b3bd281fad1394682044b
Aaron Bowes <aaron.bowes@outlook.com>
2020-09-15 13:28:33 -0400

    Add calculator functionality

--- calculator.js
+++ calculator.js
@@ -1,23 +1,58 @@
 let display = document.querySelector('#result');
-let numberButtons = document.querySelectorAll('.number');
-
+// Initially set as empty strings to check for extra decimal points later
+let firstNumber = '';
+let secondNumber = '';
+let numberButtons =  document.querySelectorAll('.number');
+let errorStatement = 'LOL math go brrr';
 for (let i = 0; i < numberButtons.length; i++) {
 	numberButtons[i].addEventListener('click', (e) => {
+		if (operator && !secondNumber) {
+			clearScreen();
+		}
+		if (display.innerText == errorStatement) {
+			clearEverything();
+		}
+		// Display number currently being punched in
 		display.innerText += e.srcElement.innerText;
+		// If operator is present, user is punching in second number
+		if (operator) {
+			secondNumber = display.innerText;
+		}
+		// Otherwise, they must be entering the first number
+		else {
+			firstNumber = display.innerText;
+		}
 	})
 }
 
 let operationButtons = document.querySelectorAll('.operator');
-let operation = '';
+let operator = '';
 for (let i = 0; i < operationButtons.length; i++) {
 	operationButtons[i].addEventListener('click', (e) => {
-		console.log(e.srcElement.name);
+		// Check if either number has more than one decimal point before continuing
+		if (firstNumber.split('.').length > 2 || secondNumber.split('.').length > 2) {
+			displayError();
+		}
+
+		// Perform arthimetic on the two numbers and "slide"  the result to the first
+		// number variable to continue the calculation
+		if (secondNumber) {
+			if (secondNumber == '0' && operator == 'divide') {
+				displayError();
+			}
+			else {
+				firstNumber = operate(+firstNumber, +secondNumber, operator);
+				display.innerText = firstNumber;
+				secondNumber = '';
+			}
+		}
+		operator = e.srcElement.name;
 	})
 }
 
 let clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', () => {
-	display.innerText = '';
+	clearEverything();
 })
 
 function add(a, b) {
@@ -46,5 +81,23 @@ function operate(a, b, operator) {
 			return multiply(a, b);
 		case "divide":
 			return divide(a, b);
+		// First number is the result of whatever operation was completed
+		case "equals" :
+			return a;
 	}
+}
+
+function clearScreen() {
+	display.innerText = '';
+}
+
+function clearEverything() {
+	firstNumber = '';
+	secondNumber = '';
+	operator = '';
+	clearScreen();
+}
+
+function displayError() {
+	display.innerText = errorStatement;
 }
